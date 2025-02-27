362188ab4783278c7fbf7448f8df9f6070c71a3d
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-02-27 09:21:24 -0500

    Initial Commit

---
+++ FuelEfficiencyCalculator/FuelEfficiencyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelEfficiencyCalculatorApp.swift
+//  FuelEfficiencyCalculator
+//
+//  Created by Nicholas Hwang on 27/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct FuelEfficiencyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
